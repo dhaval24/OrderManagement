ca12382677a635a3f57e28d69b0781b86d63af4f
Dhaval Doshi <dhaval.doshi@microsoft.com>
2019-03-21 16:22:38 -0700

    update docker-compose.yml

--- OrderCreationService/src/main/java/com/ordermanagement/OrderCreationService/config/RabbitConfig.java
+++ OrderCreationService/src/main/java/com/ordermanagement/OrderCreationService/config/RabbitConfig.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,9 @@ public class RabbitConfig {
 
     public static final String QUEUE_ORDERS = "orders-queue";
     public static final String EXCHANGE_ORDERS = "orders-exchange";
+
+    @Value("${spring.rabbitmq.host}")
+    private String hostName;
 
     @Bean
     Queue ordersQueue() {
@@ -40,6 +44,7 @@ public class RabbitConfig {
         CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
         connectionFactory.setPublisherConfirms(true);
         connectionFactory.setPublisherReturns(true);
+        connectionFactory.setHost(hostName);
         return connectionFactory;
     }
 
