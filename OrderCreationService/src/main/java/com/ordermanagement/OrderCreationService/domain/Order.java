31739330a6e207dc0f57ba75694e26bb38a45de4
dhaval24 <dhavaldoshi24@gmail.com>
2019-03-27 08:12:30 -0700

    fix bugs for postgres unable to start

--- OrderCreationService/src/main/java/com/ordermanagement/OrderCreationService/domain/Order.java
+++ OrderCreationService/src/main/java/com/ordermanagement/OrderCreationService/domain/Order.java
@@ -5,12 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 /**
  * An entity class that represents order.
  */
 @Entity
+@Table(name="ordertable")
 public class Order implements Serializable {
 
     /**
