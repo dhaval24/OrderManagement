21c27f258e23c4f938688f293c14dcfe464b4a8e
dhaval24 <dhavaldoshi24@gmail.com>
2019-03-27 14:32:51 -0700

    modify start.sh and dockerfile to run postgres aptly

--- OrderCreationService/start.sh
+++ OrderCreationService/start.sh
@@ -1,1 +1,3 @@
-#! /bin/bashjava -jar app.jar
+#! /bin/sh
+./wait-for-it.sh postgres:5432 -t 15 echo "postgres turned on!"
+java -javaagent:applicationinsights-agent-2.4.0-BETA-SNAPSHOT.jar -jar app.jar
